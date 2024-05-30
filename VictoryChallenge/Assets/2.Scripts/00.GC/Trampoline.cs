47af9a46b8ea4eba43f981b8a23764a4c5b49253
blase148 <151798999+blase148@users.noreply.github.com>
2024-05-30 15:33:15 +0900

    fix maraton
    trampoline

---
+++ VictoryChallenge/Assets/2.Scripts/00.GC/Trampoline.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trampoline : MonoBehaviour
+{
+    public float force;
+    public float arcHeight;
+    private TestMove testMove;
+    public Vector3 des;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.layer == LayerMask.NameToLayer("Player"))
+        {
+            Rigidbody rb = other.GetComponent<Rigidbody>();
+            testMove = other.GetComponent<TestMove>();
+
+            if(rb != null && testMove != null)
+            {
+                testMove.enabled = false;
+
+                Vector3 start = transform.forward;
+                Vector3 dir = (start - other.transform.position).normalized;
+                float distance = Vector3.Distance(other.transform.position, des);
+
+                Vector3 forceDir = dir + Vector3.up * Mathf.Sqrt(2 * arcHeight * Physics.gravity.magnitude / Mathf.Pow(distance / force, 2));
+                rb.velocity = forceDir * force;
+            }
+        }
+    }
+
+    
+}
