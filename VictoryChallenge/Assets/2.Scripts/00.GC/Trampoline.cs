22dd26e6f00081ad31a1df7f2b0bcee5e307782e
blase148 <151798999+blase148@users.noreply.github.com>
2024-05-30 21:12:35 +0900

    fix trampoline

--- VictoryChallenge/Assets/2.Scripts/00.GC/Trampoline.cs
+++ VictoryChallenge/Assets/2.Scripts/00.GC/Trampoline.cs
@@ -9,7 +9,6 @@ public class Trampoline : MonoBehaviour
     public float arcHeight;
     private TestMove testMove;
     public Vector3 des;
-    public Transform destination;
 
     private void OnTriggerEnter(Collider other)
     {
@@ -22,13 +21,10 @@ public class Trampoline : MonoBehaviour
             {
                 testMove.enabled = false;
 
-                Vector3 start = transform.forward;
-                Vector3 dir = (start - other.transform.position).normalized;
-                //dir = transform.forward;
-                //float distance = Vector3.Distance(destination.position, transform.position);
+                
                 float distance = Vector3.Distance(other.transform.position, des);
 
-                Vector3 forceDir = dir + Vector3.up * Mathf.Sqrt(2 * arcHeight * Physics.gravity.magnitude / Mathf.Pow(distance / force, 2));
+                Vector3 forceDir = transform.right + Vector3.up * Mathf.Sqrt(2 * arcHeight * Physics.gravity.magnitude / Mathf.Pow(distance / force, 2));
                 rb.velocity = forceDir * force;
             }
         }
