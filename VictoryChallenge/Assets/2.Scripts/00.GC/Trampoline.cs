bd079c52c194dc10bcef3a0386ac996e57e97e80
blase148 <151798999+blase148@users.noreply.github.com>
2024-05-30 16:53:16 +0900

    nevmesh link
    try...

--- VictoryChallenge/Assets/2.Scripts/00.GC/Trampoline.cs
+++ VictoryChallenge/Assets/2.Scripts/00.GC/Trampoline.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Trampoline : MonoBehaviour
@@ -8,6 +9,7 @@ public class Trampoline : MonoBehaviour
     public float arcHeight;
     private TestMove testMove;
     public Vector3 des;
+    public Transform destination;
 
     private void OnTriggerEnter(Collider other)
     {
@@ -22,6 +24,8 @@ public class Trampoline : MonoBehaviour
 
                 Vector3 start = transform.forward;
                 Vector3 dir = (start - other.transform.position).normalized;
+                //dir = transform.forward;
+                //float distance = Vector3.Distance(destination.position, transform.position);
                 float distance = Vector3.Distance(other.transform.position, des);
 
                 Vector3 forceDir = dir + Vector3.up * Mathf.Sqrt(2 * arcHeight * Physics.gravity.magnitude / Mathf.Pow(distance / force, 2));
