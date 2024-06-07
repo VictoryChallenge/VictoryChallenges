fc04f9a64a00295143ac72fa7847b045f110a930
Cherrymmy <156633614+Cherrymmy@users.noreply.github.com>
2024-06-07 17:15:15 +0900

    Add & Fixxing
    - Add soundslider
    - Main Scene fixxing

--- VictoryChallenge/Assets/2.Scripts/00.CL/GameButtonManager.cs
+++ VictoryChallenge/Assets/2.Scripts/00.CL/GameButtonManager.cs
@@ -10,11 +10,13 @@ namespace VictoryChallenge.Scripts.CL
     public class GameButtonManager : MonoBehaviour
     {
         public Toggle sound;
+        public Slider volumeslider;
         public Button exit;
         public Sprite sound_on;
         public Sprite sound_off;
+        public AudioSource _audiosource;
 
-        private GameObject _pause; 
+        private GameObject _pause;
 
         void Start()
         {
@@ -27,6 +29,24 @@ namespace VictoryChallenge.Scripts.CL
 
             // 토글 값이 변경될 때 호출될 콜백 설정
             sound.onValueChanged.AddListener(UpdateToggleImage);
+
+            if (_audiosource == null)
+            {
+                Debug.LogError("AudioSource not assigned");
+            }
+            else
+            {
+                // 초기 토글 상태에 따른 이미지 설정
+                UpdateToggleImage(sound.isOn);
+
+                float savedVolume = PlayerPrefs.GetFloat("Volume", 1f);
+                volumeslider.value = savedVolume;
+                _audiosource.volume = savedVolume;
+
+                Debug.Log("Initial volume set to: " + savedVolume);
+            }
+
+            volumeslider.onValueChanged.AddListener(UpdateVolume);
         }
 
         // Update is called once per frame
@@ -35,7 +55,7 @@ namespace VictoryChallenge.Scripts.CL
             if (Input.GetKeyDown(KeyCode.Escape)) 
             {
                 _pause.SetActive(!_pause.activeSelf);
-            }        
+            }
         }
 
         void ReturnMain()
@@ -47,6 +67,17 @@ namespace VictoryChallenge.Scripts.CL
         void UpdateToggleImage(bool isOn)
         {
             sound.image.sprite = isOn ? sound_on : sound_off;
+            _audiosource.mute = !isOn;
+        }
+
+        void UpdateVolume(float volume)
+        {
+            _audiosource.volume = volume;
+            PlayerPrefs.SetFloat("Volume", volume);  // 볼륨 값 저장
+            PlayerPrefs.Save();
+
+            // Debug.Log로 볼륨 값 확인
+            Debug.Log("Volume updated: " + volume + "dd" + _audiosource.volume);
         }
     }
 }
