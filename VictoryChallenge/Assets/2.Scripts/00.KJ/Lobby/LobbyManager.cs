721fbc18d6dd20206a1a1fe42dda77b49f6d9b33
LKJ <rickyjun94@gmail.com>
2024-06-11 12:06:57 +0900

    Fix
    Fixed Scripts

--- VictoryChallenge/Assets/2.Scripts/00.KJ/Lobby/LobbyManager.cs
+++ VictoryChallenge/Assets/2.Scripts/00.KJ/Lobby/LobbyManager.cs
@@ -6,11 +6,42 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using VictoryChallenge.KJ.Photon;
 
 namespace VictoryChallenge.KJ.Lobby
 {
     public class LobbyManager : MonoBehaviourPunCallbacks
     {
+
+        #region Singleton
+        public static LobbyManager Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new GameObject(typeof(PhotonNetwork).Name).AddComponent<LobbyManager>();
+                    DontDestroyOnLoad(_instance.gameObject);
+                }
+                return _instance;
+            }
+        }
+
+        private static LobbyManager _instance;
+
+        void Awake()
+        {
+            PhotonNetwork.GameVersion = "0.1.0";
+            Debug.Log("게임버전" + PhotonNetwork.GameVersion);
+
+            if (!PhotonNetwork.IsConnected)                 // 연결에 실패했을시 다시 연결함
+            {
+                bool isConnected = PhotonNetwork.ConnectUsingSettings();
+                Debug.Log("ConnectUsingSettings " + isConnected);
+            }
+        }
+        #endregion
+
         #region Lobby
         public override void OnLeftRoom()                   // 로비(룸)에서 떠났으면 호출
         {
