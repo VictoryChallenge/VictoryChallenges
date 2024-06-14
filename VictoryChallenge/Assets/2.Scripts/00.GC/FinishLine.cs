db1e43409065dcca088c960014499950879a3254
blase148 <151798999+blase148@users.noreply.github.com>
2024-06-14 14:47:59 +0900

    add Fallguys like map

--- VictoryChallenge/Assets/2.Scripts/00.GC/FinishLine.cs
+++ VictoryChallenge/Assets/2.Scripts/00.GC/FinishLine.cs
@@ -4,13 +4,24 @@ using UnityEngine;
 
 public class FinishLine : MonoBehaviour
 {
-    float _currentLap = 1;
+    float _currentLap = 0;
+
+    private void Start()
+    {
+        gameObject.SetActive(false);
+    }
+
 
     private void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.layer == LayerMask.NameToLayer("Player"))
         {
             _currentLap++;
+
+            if( _currentLap >= 3 )
+            {
+                _currentLap = 3;
+            }
         }
     }
 }
