bc168311b30e543d89b9a58a83048f6a8bfe7679
blase148 <151798999+blase148@users.noreply.github.com>
2024-06-15 17:22:36 +0900

    Fix Map1
    Flower Grass etc... add

---
+++ VictoryChallenge/Assets/2.Scripts/00.GC/Goal.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace VictoryChallenge.Map
+{
+    public class Goal : MonoBehaviour
+    {
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if(other.gameObject.layer == LayerMask.NameToLayer("Player"))
+            {
+
+            }
+        }
+    }
+}
