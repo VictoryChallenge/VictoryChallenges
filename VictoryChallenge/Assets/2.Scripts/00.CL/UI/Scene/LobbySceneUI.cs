748ef9afadee8ccdc9058f14ce4a7c64351c8c2a
Cherrymmy <156633614+Cherrymmy@users.noreply.github.com>
2024-06-16 16:44:25 +0900

    Add
    - Add PunRPC Image Changing
    - Fix Lobby Scene

--- VictoryChallenge/Assets/2.Scripts/00.CL/UI/Scene/LobbySceneUI.cs
+++ VictoryChallenge/Assets/2.Scripts/00.CL/UI/Scene/LobbySceneUI.cs
@@ -84,8 +84,29 @@ namespace VictoryChallenge.Scripts.CL
                 stageSelectButtonImage.sprite = newSprite;
             if (name != null)
                 GetTextMeshPro((int)TMPs.StageName).text = name;
+
+            PhotonView photonView = GetComponent<PhotonView>();
+            // 이미지 및 텍스트 변경을 모든 클라이언트에 전파
+            photonView.RPC("RPC_UpdateStageSelectTextSprite", RpcTarget.All, newSprite.name, name);
         }
-        
+
+        [PunRPC]
+        private void RPC_UpdateStageSelectTextSprite(string spriteName, string name)
+        {
+            // Resources 폴더에서 스프라이트 로드
+            Sprite sprite = Resources.Load<Sprite>(spriteName);
+            if (sprite != null)
+            {
+                stageSelectButtonImage.sprite = sprite;
+            }
+            else
+            {
+                Debug.LogError("스프라이트를 로드할 수 없습니다: " + spriteName);
+            }
+
+            GetTextMeshPro((int)TMPs.StageName).text = name;
+        }
+
         public void LeftLobby()
         {
             RoomMananger.Instance.LeaveRoom();
