46a9472bde6acb13eef4be10e467ae7b17488c22
Hyungseokkk <94478315+Hyungseokkk@users.noreply.github.com>
2024-06-17 17:16:32 +0900

    Add Finish-Line Test

--- VictoryChallenge/Assets/2.Scripts/00.HS/Finish.cs
+++ VictoryChallenge/Assets/2.Scripts/00.HS/Finish.cs
@@ -1,6 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.Rendering;
+using TMPro;
 using UnityEngine;
 using CharacterController = VictoryChallenge.Controllers.Player.CharacterController;
 
@@ -9,10 +9,13 @@ namespace VictoryChallenge.Scripts.HS
     public class Finish : MonoBehaviour
     {
         private int _rankCount;
+        private int _maxCount = 10;
+
+        [SerializeField] public TextMeshProUGUI _countText;
 
         void Start()
         {
-        
+            //_countText.enabled = false;
         }
 
         void Update()
@@ -28,8 +31,33 @@ namespace VictoryChallenge.Scripts.HS
                 {
                     _rankCount++;
                     other.gameObject.GetComponent<CharacterController>().isFinished = true;
+
+                    if(_rankCount == 1)
+                    {
+                        StartCoroutine(C_FinishCount());
+                    }
                 }
                 Debug.Log("들어온 플레이어 수 : " + _rankCount);
+            }
+        }
+
+        IEnumerator C_FinishCount()
+        {
+            bool isCheck = true;
+            _countText.enabled = true;
+            _countText.text = _maxCount.ToString();
+
+            while (isCheck)
+            {
+                yield return new WaitForSeconds(1f);    
+                _maxCount--;
+                _countText.text = _maxCount.ToString();
+
+                if (_maxCount == 0)
+                {
+                    isCheck = false;
+                    _countText.text = "Game Over";
+                }
             }
         }
     }
