fc0bf6358515441d9958a37bccf7cb9f22848a32
blase148 <151798999+blase148@users.noreply.github.com>
2024-06-19 16:07:43 +0900

    add effect

--- VictoryChallenge/Assets/2.Scripts/00.GC/FallRespawn.cs
+++ VictoryChallenge/Assets/2.Scripts/00.GC/FallRespawn.cs
@@ -5,12 +5,15 @@ using UnityEngine;
 public class FallRespawn : MonoBehaviour
 {
     [SerializeField] private Vector3 _respawnPoint;
+    [SerializeField] private GameObject _respawnEffect;
 
     private void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.layer == LayerMask.NameToLayer("Player"))
         {
             other.gameObject.transform.position = _respawnPoint;
+
+            Instantiate(_respawnEffect, other.transform);
         }
     }
 }
