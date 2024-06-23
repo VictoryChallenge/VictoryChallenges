18f01e9d6ed3a50f9466dfd27ae57957c5dbd464
hyungseokk <61190500+hyungseokk@users.noreply.github.com>
2024-06-24 00:49:21 +0900

    Add PlayerCustomProperty : Score

--- VictoryChallenge/Assets/2.Scripts/00.CL/GameManagerCL.cs
+++ VictoryChallenge/Assets/2.Scripts/00.CL/GameManagerCL.cs
@@ -5,12 +5,28 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using VictoryChallenge.KJ.Photon;
 using Photon.Realtime;
+using VictoryChallenge.KJ.Database;
+using ExitGames.Client.Photon.StructWrapping;
+using UnityEngine.SocialPlatforms.Impl;
+using System.Collections.Generic;
 
 namespace VictoryChallenge.Scripts.CL
 {
     public class GameManagerCL : MonoBehaviourPunCallbacks
     {
         // 이 스크립트는 모든 플레이어가 씬을 로드했는지 확인하고, 그 후 게임을 시작하는 역할을 합니다.
+
+        public enum Point
+        {
+            first = 10,
+            second = 8,
+            third = 6,
+            fourth = 5,
+            fifth = 4,
+            sixth = 3,
+            seventh = 2,
+            eighth = 1,
+        }
 
         private void Start()
         {
@@ -25,7 +41,8 @@ namespace VictoryChallenge.Scripts.CL
                 // 플레이어의 씬 로드 상태를 CustomProperties에 설정
                 Hashtable props = new Hashtable
                 {
-                    { "SceneLoaded", true }
+                    { "SceneLoaded", true },
+                    { "Score", 0 }
                 };
                 PhotonNetwork.LocalPlayer.SetCustomProperties(props);
             }
@@ -40,7 +57,97 @@ namespace VictoryChallenge.Scripts.CL
                 // SceneLoaded가 변경되었을 때만 RPC 호출
                 photonView.RPC("PlayerLoadedScene", RpcTarget.All);
             }
-        }       
+
+            int debugScore;
+            if (changedProps.ContainsKey("Score"))
+            {
+                if (targetPlayer.CustomProperties.TryGetValue("Score", out debugScore))
+                {
+                    Debug.Log("Changed Score : " + debugScore);
+                }
+            }
+        }
+
+        public int RewardPoint(int rank)
+        {
+            Point point;
+
+            switch (rank)
+            {
+                case 1:
+                    point = Point.first;
+                    break;
+                case 2:
+                    point = Point.second;
+                    break;
+                case 3:
+                    point = Point.third;
+                    break;
+                case 4:
+                    point = Point.fourth;
+                    break;
+                case 5:
+                    point = Point.fifth;
+                    break;
+                case 6:
+                    point = Point.sixth;
+                    break;
+                case 7:
+                    point = Point.seventh;
+                    break;
+                case 8:
+                    point = Point.eighth;
+                    break;
+                default:
+                    point = 0;
+                    break;
+            }
+
+            //Debug.Log("point : " + (int)point);
+            return (int)point;
+        }
+
+        public void SetRank(int rank)
+        {
+            int score = RewardPoint(rank);
+
+            Debug.Log($"Local NickName : {PhotonNetwork.LocalPlayer.NickName}" + "'s score : " + score);
+
+            if (PhotonNetwork.PlayerList.All(player => player.CustomProperties.ContainsKey("Score")))
+            {
+                Debug.Log("PhotonNetwork.PlayerList : " + PhotonNetwork.PlayerList.ToStringFull());
+            }
+            
+            PhotonNetwork.LocalPlayer.SetCustomProperties(new ExitGames.Client.Photon.Hashtable { { "Score", score } });
+        }
+
+        public int ChooseWinner()
+        {
+            List<int> scores = new List<int>();
+
+            foreach (var player in PhotonNetwork.PlayerList)
+            {
+                if(player.CustomProperties.ContainsKey("Score"))
+                {
+                    scores.Add((int)player.CustomProperties["Score"]);
+                }
+            }
+
+            scores.OrderByDescending(x => x).ToList();
+
+            //foreach(var player in PhotonNetwork.PlayerList)
+            //{
+            //    if(player.CustomProperties.ContainsValue())
+            //}
+            foreach(int score  in scores)
+            {
+                Debug.Log("score in List : " + score);
+            }
+
+            Debug.Log("Sorted Score" + scores.First());
+
+            return scores.First();
+        }
 
         // 마스터 클라이언트가 각 플레이어의 상태를 확인하고, 모든 플레이어가 씬을 로드했으면 게임을 시작합니다.
         [PunRPC]
