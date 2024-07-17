68ffc4c34fefac104006c9397bf605ee8d31baef
LKJ <rickyjun94@gmail.com>
2024-07-17 15:16:02 +0900

    Feat : Add Map In Stage3
    2 Players In Stage

--- VictoryChallenge/Assets/2.Scripts/00.KJ/00.Map/KJMap2p.cs
+++ VictoryChallenge/Assets/2.Scripts/00.KJ/00.Map/KJMap2p.cs
@@ -2,14 +2,8 @@ using UnityEngine;
 
 namespace VictoryChallenge.KJ.Map
 {
-    // 싱글톤으로 할지 말지는 고민좀
     public class KJMap2p : MonoBehaviour
     {
-        /*
-         *  장애물 속도 태그 및 레이어로 해결하기
-         *  기둥 3개 패턴 다른걸로 고치거나 고쳐보기
-         *  컷씬제작
-         *  포톤 및 플레이어 스폰매니저 연결
-         */
+
     }
 }
---
+++ VictoryChallenge/Assets/2.Scripts/00.KJ/00.Map/ObstacleManager.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using UnityEngine;
+
+namespace VictoryChallenge.KJ.Map
+{
+    public class ObstacleManager : MonoBehaviour
+    {
+        public GameObject[] obstaclePrefabs;
+        public Transform spawnPointRed;
+        public Transform spawnPointBlue;
+        public float spawnInterval = 1.5f;
+
+        void Start()
+        {
+            StartCoroutine(SpawnObstacles());
+        }
+
+        private IEnumerator SpawnObstacles()
+        {
+            while (true)
+            {
+                SpawnRandomObstacles();
+                yield return new WaitForSeconds(spawnInterval);
+            }
+        }
+
+        private void SpawnRandomObstacles()
+        {
+            int randomIndex = Random.Range(0, obstaclePrefabs.Length);
+            GameObject selectedPrefab = obstaclePrefabs[randomIndex];
+            Instantiate(selectedPrefab, spawnPointRed.position, spawnPointRed.rotation);
+            Instantiate(selectedPrefab, spawnPointBlue.position, spawnPointBlue.rotation);
+        }
+    }
+
+}
