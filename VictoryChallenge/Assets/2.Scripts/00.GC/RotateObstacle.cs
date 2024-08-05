65a75f4e92705947f86601d2395dddf68f304235
blase148 <151798999+blase148@users.noreply.github.com>
2024-08-05 12:42:09 +0900

    Fix RotateObstacle
    캐릭들 날아다니는거 고쳤다

--- VictoryChallenge/Assets/2.Scripts/00.GC/RotateObstacle.cs
+++ VictoryChallenge/Assets/2.Scripts/00.GC/RotateObstacle.cs
@@ -1,26 +1,40 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-
-namespace VictoryChallenge.Map
+public class RotateObstacle : MonoBehaviour
 {
-    public class RotateObstacle : MonoBehaviour
-    {
-        private void OnCollisionEnter(Collision collision)
-        {
-            if (collision.gameObject.layer == LayerMask.NameToLayer("Player"))
-            {
-                collision.transform.SetParent(transform);
-            }
-        }
+    private Vector3 _previousRotation;
+    private Transform _playerTransform;
 
-        private void OnCollisionExit(Collision collision)
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Player"))
         {
-            if (collision.gameObject.layer == LayerMask.NameToLayer("Player"))
-            {
-                collision.transform.SetParent(null);
-            }
+            _playerTransform = collision.transform;
+            _previousRotation = transform.rotation.eulerAngles;
+        }
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Player"))
+        {
+            // 플랫폼의 회전 변화량 계산
+            Vector3 currentRotation = transform.rotation.eulerAngles;
+            float rotationDelta = currentRotation.y - _previousRotation.y;
+
+            // 회전 변화량만큼 플레이어를 중심을 기준으로 회전
+            _playerTransform.RotateAround(transform.position, Vector3.up, rotationDelta);
+
+            // 이전 회전 값 업데이트
+            _previousRotation = currentRotation;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Player"))
+        {
+            _playerTransform = null;
         }
     }
 }
