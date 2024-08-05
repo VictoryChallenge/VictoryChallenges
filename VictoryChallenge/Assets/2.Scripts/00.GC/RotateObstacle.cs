1b369dd9b1a9e70137538641a0b366897dbc615c
Cherrymmy <156633614+Cherrymmy@users.noreply.github.com>
2024-08-05 13:56:04 +0900

    Fix
    - Fix JustRun2, 3
    - Fix RotateObstacle.cs

--- VictoryChallenge/Assets/2.Scripts/00.GC/RotateObstacle.cs
+++ VictoryChallenge/Assets/2.Scripts/00.GC/RotateObstacle.cs
@@ -1,40 +1,56 @@
+using Photon.Pun;
 using UnityEngine;
 
 public class RotateObstacle : MonoBehaviour
 {
     private Vector3 _previousRotation;
     private Transform _playerTransform;
+    private PhotonView _pv;
+
+    private void Awake()
+    {
+        _pv = GetComponent<PhotonView>();
+    }
 
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.layer == LayerMask.NameToLayer("Player"))
+        if (_pv.IsMine)
         {
-            _playerTransform = collision.transform;
-            _previousRotation = transform.rotation.eulerAngles;
+            if (collision.gameObject.layer == LayerMask.NameToLayer("Player"))
+            {
+                _playerTransform = collision.transform;
+                _previousRotation = transform.rotation.eulerAngles;
+            }
         }
     }
 
     private void OnCollisionStay(Collision collision)
     {
-        if (collision.gameObject.layer == LayerMask.NameToLayer("Player"))
+        if (_pv.IsMine)
         {
-            // 플랫폼의 회전 변화량 계산
-            Vector3 currentRotation = transform.rotation.eulerAngles;
-            float rotationDelta = currentRotation.y - _previousRotation.y;
+            if (collision.gameObject.layer == LayerMask.NameToLayer("Player"))
+            {
+                // 플랫폼의 회전 변화량 계산
+                Vector3 currentRotation = transform.rotation.eulerAngles;
+                float rotationDelta = currentRotation.y - _previousRotation.y;
 
-            // 회전 변화량만큼 플레이어를 중심을 기준으로 회전
-            _playerTransform.RotateAround(transform.position, Vector3.up, rotationDelta);
+                // 회전 변화량만큼 플레이어를 중심을 기준으로 회전
+                _playerTransform.RotateAround(transform.position, Vector3.up, rotationDelta);
 
-            // 이전 회전 값 업데이트
-            _previousRotation = currentRotation;
+                // 이전 회전 값 업데이트
+                _previousRotation = currentRotation;
+            }
         }
     }
 
     private void OnCollisionExit(Collision collision)
     {
-        if (collision.gameObject.layer == LayerMask.NameToLayer("Player"))
+        if (_pv.IsMine)
         {
-            _playerTransform = null;
+            if (collision.gameObject.layer == LayerMask.NameToLayer("Player"))
+            {
+                _playerTransform = null;
+            }
         }
     }
 }
