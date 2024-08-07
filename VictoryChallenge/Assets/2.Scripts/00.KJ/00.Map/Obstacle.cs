cdfc220a073e43e0de09cdee718c64d4837ee59f
LKJ <rickyjun94@gmail.com>
2024-08-07 11:53:24 +0900

    WIP : Obstacle 6
    fixed RIgidbody in Script

--- VictoryChallenge/Assets/2.Scripts/00.KJ/00.Map/Obstacle.cs
+++ VictoryChallenge/Assets/2.Scripts/00.KJ/00.Map/Obstacle.cs
@@ -5,12 +5,35 @@ namespace VictoryChallenge.KJ.Map
     public class Obstacle : MonoBehaviour
     {
         public float minYPoisition = -5f;
+        private Rigidbody rb;
+
+        void Start()
+        {
+            rb = GetComponent<Rigidbody>();
+        }
 
         void Update()
         {
             if (transform.position.y < minYPoisition)
             {
                 Destroy(gameObject);
+            }
+        }
+
+        void OnCollisionStay(Collision collision)
+        {
+            if (collision.gameObject.CompareTag("Ground"))
+            {
+                rb.constraints = RigidbodyConstraints.FreezePositionY;
+            }
+        }
+
+        void OnCollisionExit(Collision collision)
+        {
+            if (collision.gameObject.CompareTag("Ground"))
+            {
+                rb.constraints = RigidbodyConstraints.None;
+                rb.constraints = RigidbodyConstraints.FreezeRotation;
             }
         }
     }
